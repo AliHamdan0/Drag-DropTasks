7bbcb6a0a6a2a65d48255736f85a47285a390a5d
ali <ali.hamdan@meta-itech.com>
2022-03-29 01:06:16 +0300

    font

--- pages/_app.js
+++ pages/_app.js
@@ -12,9 +12,11 @@ function MyApp({ Component, pageProps }) {
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
+            href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700&family=El+Messiri:wght@400;500;600;700&family=Inter:wght@400;500;600;700;800&display=swap"
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
         </Head>
         <Component {...pageProps} />
